3c8ace1b2dc8c2b9de31e2d10c6d805e1cdf6de6
Ian Nguyen <ian.nguyen@procore.com>
2017-09-27 09:40:17 -0700

    add git and oh-my-zsh. remove duplicate from mac script

--- linux/core_install.sh
+++ linux/core_install.sh
@@ -32,6 +32,29 @@ echo "============================================================"
 sudo apt install vlc -y
 
 echo "============================================================"
+echo "INSTALLING GIT"
+echo "============================================================"
+sudo apt install git -y
+
+echo "============================================================"
+echo "INSTALLING ZSH"
+echo "============================================================"
+sudo apt install zsh -y
+
+echo "============================================================"
+echo "INSTALLING OH-MY-ZSH"
+echo "============================================================"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo "============================================================"
+echo "CLONING IANNGUYEN/CFG"
+echo "============================================================"
+git clone https://github.com/iannguyen/cfg.git ~/Desktop/cfg
+mkdir ~/.oh-my-zsh/custom/themes
+ln -s ~/Desktop/cfg/.oh-my-zsh/custom/themes/mein.zsh-theme .oh-my-zsh/custom/themes/mein.zsh-theme
+ln -sf ~/Desktop/cfg/.zshrc
+
+echo "============================================================"
 echo "RUNNING APT AUTOREMOVE"
 echo "============================================================"
 sudo apt autoremove
