8bd526c2c702fe81aa5c687e46af119e991a9def
Ian Nguyen <ian.nguyen@procore.com>
2017-10-01 22:36:43 -0700

    remove old stuff

--- linux/core_install.sh
+++ linux/core_install.sh
@@ -31,11 +31,8 @@ echo "============================================================"
 echo "INSTALLING SOFTWARE-PROPERTIES-COMMON"
 echo "============================================================"
 sudo apt install software-properties-common -y
-sudo apt install python-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo apt install python-dev python-pip python3-dev python3-pip -y
-sudo apt install python3-setuptools -y
-sudo easy_install3 pip
 sudo pip3 install neovim
 source ~/.zshrc
 
