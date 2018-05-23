7e1ec98b49f9107d88147fd2adef75b090bc5497
Ian Nguyen <ian.nguyen@procore.com>
2018-05-22 20:59:51 -0700

    delete some things

--- linux/apps_install.sh
+++ linux/apps_install.sh
@@ -18,31 +18,7 @@ echo "============================================================"
 sudo apt install unity-tweak-tool -y
 
 echo "============================================================"
-echo "GNOME TWEAK TOOL"
-echo "============================================================"
-sudo apt install gnome-tweak-tool -y
-
-echo "============================================================"
-echo "REDSHIFT"
-echo "============================================================"
-sudo apt install redshift redshift-gtk -y
-
-echo "============================================================"
 echo "IMWHEELRC"
 echo "============================================================"
 sudo apt install imwheel -y
 ln -sf ~/Desktop/cfg/linux/.imwheelrc
-
-echo "============================================================"
-echo "TOUCHPAD INDICATOR"
-echo "============================================================"
-sudo add-apt-repository ppa:atareao/atareao
-sudo apt update
-sudo apt install touchpad-indicator
-
-echo "============================================================"
-echo "INSTALLING GNOME TERMINAL"
-echo "============================================================"
-# sudo apt install gnome-terminal -y
-
-
